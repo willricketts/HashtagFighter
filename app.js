6d39e31503ade0ad171d8e4d7a3f7639082ec359
Will <will@willricketts.com>
2014-05-01 10:59:12 -0400

    Ditched export for simplicity. Added some front end logic

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ var port = 3000;
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var twitterKeys = require('./modules/twitterkeys.js');
+var T = new Twit(twitterKeys);
 
 //Set up Twitter wrapper with twitterKeys module exports
-var T = new Twit(twitterKeys);
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -29,7 +29,36 @@ if ('development' == app.get('env')) {
 }
 
 //Routes
-app.get('/', routes.index);
+app.get('/', function(req, res){
+  var clients = io.sockets.clients();
+  console.log(clients.id);
+  var player1 = "",
+      player2 = "";
+
+  //Once players are selected
+  if((req.query.player1 != "") && (req.query.player2 != "")){
+    player1 = req.query.player1,
+    player2 = req.query.player2;
+    console.log(player1 + " | " + player2);
+
+    var hashtags = [player1, player2];
+    io.sockets.on('connection', function(socket){
+      console.log('client connected');
+      console.log('streaming tweets for: ' + hashtags[0] + " & " + hashtags[1]);
+      var options = { track : hashtags, language: 'en' };
+      var stream = T.stream('statuses/filter', options);
+      stream.on('tweet', function (tweet) {
+        socket.emit('twitter', tweet);
+      });
+    });
+    io.sockets.on('disconnect', function(socket) {
+      socket.socket.disconnect();
+    });
+  };
+
+  //Render view
+  res.render('index', { title: 'Hashtag Fighter', player1: player1, player2: player2 });
+});
 
 server.listen(port, function(){
     console.log("Server listening on port " + port + ". HADOUKEN!");
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,25 @@
 exports.index = function(req, res){
   //Take query strings and assign them to player objects
-  var player1 = req.query.player1,
-      player2 = req.query.player2;
-  console.log(player1 + " | " + player2);
-  res.render('index', { title: 'Hashtag Fighter', player1: player1, player2: player2 });
+var clients = io.sockets.clients();
+console.log(clients.id);
+var player1 = req.query.player1,
+    player2 = req.query.player2;
+console.log(player1 + " | " + player2);
+
+var hashtags = [player1, player2];
+io.sockets.on('connection', function(socket){
+  console.log('client connected');
+  console.log('streaming tweets for: ' + hashtags[0] + " & " + hashtags[1]);
+  var options = { track : hashtags, language: 'en' };
+  var stream = T.stream('statuses/filter', options);
+  stream.on('tweet', function (tweet) {
+    socket.emit('twitter', tweet);
+  });
+});
+io.sockets.on('disconnect', function(socket) {
+  socket.socket.disconnect();
+});
+res.render('index', { title: 'Hashtag Fighter', player1: player1, player2: player2 });
+
+
 };
