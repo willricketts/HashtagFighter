45fb8a4712d8dd3fec5743d9827a607692947a67
Will <will@willricketts.com>
2014-05-01 09:41:12 -0400

    Added basic player display logic

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
-
-/*
- * GET home page.
- */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'Hashtag Fighter' });
+  //Take query strings and assign them to player objects
+  var player1 = req.query.player1,
+      player2 = req.query.player2;
+  console.log(player1 + " | " + player2);
+  res.render('index', { title: 'Hashtag Fighter', player1: player1, player2: player2 });
 };
